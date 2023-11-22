3e0c04d0bc70961d75166618261890e5c59105e4
Adrian Dziekonski <adrian.dziekonski@onet.pl>
2023-11-22 13:49:35 +0100

    Piwrwszy model value

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Facebook.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facebook.API.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) {}  
+
+        public  DbSet<Value> Values { get; set; }  
+        ////To jest tabela do bazy danych
+        
+    }
+}
