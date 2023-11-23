dd4cc9891de7f11ef96811be318bb77c03d2776e
Adrian Dziekonski <adrian.dziekonski@onet.pl>
2023-11-23 13:31:54 +0100

    Pobieranie z db

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Facebook.API.Data;
+using Facebook.API.Models;
 
 namespace Facebook.API.Controllers
 {   //http://localhost:5000/api/values
@@ -10,36 +12,60 @@ namespace Facebook.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext contextRO;
+        public ValuesController(DataContext context)
+        {
+            contextRO = context;
+            
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValuesAll()
         {
-            return new string[] { "value1", "value2" };
+            var values=contextRO.Values.ToList();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+           var value=contextRO.Values.FirstOrDefault(x=>x.Id==id);
+           return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult AddValue([FromBody] Value value)
         {
+            contextRO.Values.Add(value);
+            contextRO.SaveChanges();
+            return Ok(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult EditValue(int id, [FromBody] Value value)
         {
+            var data=contextRO.Values.Find(id);
+            data.Name=value.Name;
+            contextRO.Values.Update(data);
+            contextRO.SaveChanges();
+            return Ok(data);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult DeleteValue(int id)
         {
+            var data =contextRO.Values.Find(id);
+
+            if(data==null)
+            return NoContent();
+            
+            contextRO.Values.Remove(data);
+            contextRO.SaveChanges();
+            return Ok(data);
         }
     }
 }
