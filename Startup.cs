9200552899da659cc749f6dc8fee533f792e31ee
Adrian Dziekonski <adrian.dziekonski@onet.pl>
2024-02-27 17:15:22 +0100

    publikacja webio

--- Startup.cs
+++ Startup.cs
@@ -39,74 +39,78 @@ namespace Facebook.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) //tu kolwjność serwisów jest bez znazcenia
         {
-            services.AddDbContext<DataContext>(x=>x.UseMySql(Configuration.GetConnectionString("DefaultConnection")).ConfigureWarnings(warnings=>warnings.Ignore(CoreEventId.IncludeIgnoredWarning)));
+            services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")).ConfigureWarnings(warnings => warnings.Ignore(CoreEventId.IncludeIgnoredWarning)));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                    .AddJsonOptions(opt=>{
-                        opt.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    .AddJsonOptions(opt =>
+                    {
+                        opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                     });
             services.AddCors();  //bład dostepu do api
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySetting"));
             services.AddAutoMapper();
             services.AddTransient<Seed>(); //klasa seed odczytuje i dodaje dane do bazy gdy jest pusta
-            services.AddScoped<IAuthRepository,AuthRepository>();  //rejestraacja repozytorium auth scoped coś dla srwisów pomiedzy lekkimi a cieższymi
+            services.AddScoped<IAuthRepository, AuthRepository>();  //rejestraacja repozytorium auth scoped coś dla srwisów pomiedzy lekkimi a cieższymi
             //dodajemy po czym ma przeprowadzić autntyfikacje i jej opcje
             //tych opcji do konca nie ogarniam 
-            services.AddScoped<IGenericRepository,GenericRepository>();
-            services.AddScoped<IUserRepository,UserRepository>();
-               
-                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                        .AddJwtBearer(options =>{
-                            options.TokenValidationParameters = new TokenValidationParameters
-                            {
-                                ValidateIssuerSigningKey = true,
-                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                                ValidateIssuer = false,
-                                ValidateAudience = false,
-                                
-                            };
+            services.AddScoped<IGenericRepository, GenericRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+
+                        };
                         IdentityModelEventSource.ShowPII = true;
-                        //showPII pokazuje błąd tokena w konsoli
+                            //showPII pokazuje błąd tokena w konsoli
 
                         });
         }
 
-         public void ConfigureDevelopmentServices(IServiceCollection services) //tu kolwjność serwisów jest bez znazcenia
+        public void ConfigureDevelopmentServices(IServiceCollection services) //tu kolwjność serwisów jest bez znazcenia
         {
-            services.AddDbContext<DataContext>(x=>x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                    .AddJsonOptions(opt=>{
-                        opt.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    .AddJsonOptions(opt =>
+                    {
+                        opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                     });
             services.AddCors();  //bład dostepu do api
             services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySetting"));
             services.AddAutoMapper();
             services.AddTransient<Seed>(); //klasa seed odczytuje i dodaje dane do bazy gdy jest pusta
-            services.AddScoped<IAuthRepository,AuthRepository>();  //rejestraacja repozytorium auth scoped coś dla srwisów pomiedzy lekkimi a cieższymi
+            services.AddScoped<IAuthRepository, AuthRepository>();  //rejestraacja repozytorium auth scoped coś dla srwisów pomiedzy lekkimi a cieższymi
             //dodajemy po czym ma przeprowadzić autntyfikacje i jej opcje
             //tych opcji do konca nie ogarniam 
-            services.AddScoped<IGenericRepository,GenericRepository>();
-            services.AddScoped<IUserRepository,UserRepository>();
-               
-                services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                        .AddJwtBearer(options =>{
-                            options.TokenValidationParameters = new TokenValidationParameters
-                            {
-                                ValidateIssuerSigningKey = true,
-                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
-                                ValidateIssuer = false,
-                                ValidateAudience = false,
-                                
-                            };
+            services.AddScoped<IGenericRepository, GenericRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+
+                        };
                         IdentityModelEventSource.ShowPII = true;
-                        //showPII pokazuje błąd tokena w konsoli
+                            //showPII pokazuje błąd tokena w konsoli
 
                         });
         }
 
         //komponenty do obsugi żądań 
-        
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,Seed seeder) //tu ważna jest klejność
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder) //tu ważna jest klejność
         {
             if (env.IsDevelopment())
             {
@@ -114,9 +118,9 @@ namespace Facebook.API
             }
             else
             {
-                app.UseExceptionHandler(builder => 
+                app.UseExceptionHandler(builder =>
                 {
-                    builder.Run(async context => 
+                    builder.Run(async context =>
                     {
                         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
@@ -130,16 +134,17 @@ namespace Facebook.API
                     });
                 });
             }
-           
+
             seeder.SeedUsers();
-            app.UseCors(x=>x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); //bład dostepu do api
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()); //bład dostepu do api
             app.UseAuthentication();
             app.UseDefaultFiles();
             app.UseStaticFiles();
-            app.UseMvc(routes=>{
+            app.UseMvc(routes =>
+            {
                 routes.MapSpaFallbackRoute(
                     name: "spa",
-                     defaults: new {controller="Fallback", action="Index"});
+                     defaults: new { controller = "Fallback", action = "Index" });
             });
         }
     }
